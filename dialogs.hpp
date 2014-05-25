148509b219b3d10970f5a976b252ed26a65c8bb7
Continuum-UNIF <continuum@arma-unif.de>
2014-05-25 23:41:20 +0200

    kleinigkeiten

---
+++ dialogs.hpp
@@ -0,0 +1,37 @@
+class test_dialog
+{
+	idd=-1;
+	movingenable=false;
+	
+	class controls 
+	{
+	////////////////////////////////////////////////////////
+// GUI EDITOR OUTPUT START (by Continuum, v1.063, #Kuwoxe)
+////////////////////////////////////////////////////////
+
+class 000_frame: RscFrame
+{
+	idc = 1800;
+	text = "frame"; //--- ToDo: Localize;
+	x = 0.304062 * safezoneW + safezoneX;
+	y = 0.247 * safezoneH + safezoneY;
+	w = 0.391875 * safezoneW;
+	h = 0.506 * safezoneH;
+};
+class 000_text: RscText
+{
+	idc = 1000;
+	text = "Schwall Gummiball"; //--- ToDo: Localize;
+	x = 0.345312 * safezoneW + safezoneX;
+	y = 0.291 * safezoneH + safezoneY;
+	w = 0.0979687 * safezoneW;
+	h = 0.077 * safezoneH;
+};
+////////////////////////////////////////////////////////
+// GUI EDITOR OUTPUT END
+////////////////////////////////////////////////////////
+
+	
+	
+};
+};
