7d18499d92419a59bf5c3579939f0a9e07ee873a
Continuum-UNIF <continuum@arma-unif.de>
2014-05-29 16:44:57 +0200

    Dialog change

--- dialogs.hpp
+++ dialogs.hpp
@@ -1,75 +1,92 @@
-/*
-Creative Commons Lizenzvertrag
-Okkupation Altis von http://www.Arma-Unif.de ist lizenziert unter einer
-Creative Commons Namensnennung - Nicht kommerziell - Keine Bearbeitungen 4.0 International Lizenz.
-http://creativecommons.org/licenses/by-nc-nd/4.0/deed.de
-
-Creative Commons License
-Okkupation Altis by http://www.Arma-Unif.de  licensed under a
-Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
-http://creativecommons.org/licenses/by-nc-nd/4.0/
-
-*/
-class dialog_waffenausgabe
+class H8ER_dialog
 {
 	idd=-1;
 	movingenable=false;
 	
 	class controls 
 	{
-////////////////////////////////////////////////////////
-// GUI EDITOR OUTPUT START (by Continuum, v1.063, #Kanavo)
-////////////////////////////////////////////////////////
+	class bg
+			{    
+				idc = 0;
+				type = 0;
+				style = 2096; 
+				x = -0.2; 
+				y = -0.2;
+				h = 1.4;
+				w = 1.4;
+				font = "EtelkaNarrowMediumPro";
+				sizeEx = 0.05;
+				colorBackground[] = {0,0,0,0}; 
+				colorText[] = {1,1,1,1}; 
+				text = "pictures\Tablet.jpg";
+				fixedWidth = 0;
+				shadow = 0;
+			}; 
+			class exit_knopf
+			{    
+				idc = 1;
+				type = 11;
+				style = 0;
+				x = 0.831; 
+				y = 0.71;
+				h = 0.7;
+				w = 0.6;
+				font = "EtelkaNarrowMediumPro";
+				sizeEx = 0.05;
+				color[] = { 1, 1, 1, 0 };
+				colorActive[] = { 1, 0.2, 0.2, 0 };
+				soundEnter[] = { "", 0, 1 };   
+				soundPush[] = { "", 0, 1 };
+				soundClick[] = { "", 0, 1 };
+				soundEscape[] = { "", 0, 1 };
+				action = "closeDialog 0;";
+				text = "O";
+				default = true;
+				
+			}; 
+			class menu_knopf_1
+			{    
+				idc = 2;
+				type = 11;
+				style = 0;
+				x = 0.0; 
+				y = 0.0;
+				h = 0.7;
+				w = 0.6;
+				font = "EtelkaNarrowMediumPro";
+				colorBackground[] = { 0.2,0.9,0.5, 0.9};
+				sizeEx = 0.05;
+				color[] = { 1, 1, 1, 1 };
+				colorActive[] = { 1, 0.2, 0.2, 1 };
+				soundEnter[] = { "", 0, 1 };  
+				soundPush[] = { "", 0, 1 };
+				soundClick[] = { "", 0, 1 };
+				soundEscape[] = { "", 0, 1 };
+				action = "closeDialog 0;";
+				text = "Aufgabenbereich";
+				default = true;
+				
+			}; 
+			class text_box_center
+			{    
+				idc = 3;
+				type = 0;
+				style = 16;
+				x = 0.0; 
+				y = 0.20;
+				h = 0.7;
+				w = 1.0;
+				font = "EtelkaNarrowMediumPro";
+				sizeEx = 0.05;
+				lineSpacing = 1;
+				colorBackground[] = {0,0,0,0}; 
+				colorText[] = {1,1,1,1}; 
+				text = "test \nHallo";
+				fixedWidth = 0;
+				shadow = 0;
+			}; 
+	
+	
+};
+};
 
-class ausgabe_frame: RscFrame
-{
-	idc = 1800;
-	text = ""; //--- ToDo: Localize;
-	x = 0.340156 * safezoneW + safezoneX;
-	y = 0.302 * safezoneH + safezoneY;
-	w = 0.33 * safezoneW;
-	h = 0.385 * safezoneH;
-};
-class ausgabe_box: BOX
-{
-	idc = 1800;
-	text = ""; //--- ToDo: Localize;
-	x = 0.340156 * safezoneW + safezoneX;
-	y = 0.302 * safezoneH + safezoneY;
-	w = 0.33 * safezoneW;
-	h = 0.385 * safezoneH;
-};
-class ausgabe_bild: RscPicture
-{
-	idc = 1200;
-	text = "pictures\unif_klein.paa";
-	x = 0.618281 * safezoneW + safezoneX;
-	y = 0.313 * safezoneH + safezoneY;
-	w = 0.035 * safezoneW;
-	h = 0.055 * safezoneH;
-};
-class ausgabe_button: RscButton
-{
-	idc = 1600;
-	text = "Verstanden"; //--- ToDo: Localize;
-	x = 0.469062 * safezoneW + safezoneX;
-	y = 0.621 * safezoneH + safezoneY;
-	w = 0.061875 * safezoneW;
-	h = 0.044 * safezoneH;
-	action = "closeDialog 0;";
-};
-class ausgabe_text: RscText
-{
-	idc = 1000;
-	text = "So... Sie sind der neue was? Gut. Ich habe Ihnen eine Kiste mit passender Ausrüstung zusammengestellt. Sie erhalten diese Kiste einmalig, verstehen Sie? EINMALIG. Bei Verlust melden Sie dies umgehend ihrem Zugführer. Da wir zur Zeit unterbesetzt sind wird ihre Ausrüstung nur alle 30 min von einem WaKa Mannschafter aufgefüllt...."; //--- ToDo: Localize;
-	x = 0.365937 * safezoneW + safezoneX;
-	y = 0.379 * safezoneH + safezoneY;
-	w = 0.278437 * safezoneW;
-	h = 0.22 * safezoneH;
-};
-////////////////////////////////////////////////////////
-// GUI EDITOR OUTPUT END
-////////////////////////////////////////////////////////
-
-};
-};
